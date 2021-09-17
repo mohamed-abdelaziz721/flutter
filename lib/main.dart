ac137f366a4e84af0dfd7c46b08a24986385c405
mohamed abd elaziz <61313695+mohamed-abdelaziz721@users.noreply.github.com>
2021-09-17 22:55:24 +0200

    Home Screen Changed to Hosting projects Page. Moved login page from home to its new directory.

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,137 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:sprints_naid_flutter/projects/sign_in_form/screens/sign_in.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false,
+  runApp(const MaterialApp(debugShowCheckedModeBanner: false,
     home:HomePage(),
   ));
 }
 
 class HomePage  extends StatelessWidget{
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Expanded(child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    const Text("Login",
-                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
-                    const SizedBox(height: 20,),
-                    Text("Login to your account",
-                      style: TextStyle(
-                          fontSize: 15,
-                          color:Colors.grey[700]),)
-                  ],
-                ),
-                Container(
-                  padding: const EdgeInsets.only(top: 100),
-                  height: 150,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("assets/background.png"),
-                        fit: BoxFit.fitHeight
+      body: SafeArea(
+        child: Container(
+          // we will give media query height
+          // double.infinity make it big as my parent allows
+          // while MediaQuery make it big as per the screen
+
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            // even space distribution
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  const Text(
+                    "Welcome",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+
                     ),
 
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  child: Column(
-                    children: <Widget>[
-                      inputFile(label: "Email"),
-                      inputFile(label: "Password", obscureText: true)
-                    ],
+                  const SizedBox(
+                    height: 5,
                   ),
-                ),
-                Padding(padding:
-                const EdgeInsets.symmetric(horizontal: 40),
-                  child: Container(
-                    padding: const EdgeInsets.only(top: 3, left: 3),
-                    decoration:
-                    BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        border: const Border(
-                          bottom: BorderSide(color: Colors.black),
-                          top: BorderSide(color: Colors.black),
-                          left: BorderSide(color: Colors.black),
-                          right: BorderSide(color: Colors.black),
-
-                        )
-
-
-
-                    ),
-                    child: MaterialButton(
-                      minWidth: double.infinity,
-                      height: 60,
-                      onPressed: () {},
-                      color: const Color(0xff0095FF),
-                      elevation: 0,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-
-                      ),
-                      child: const Text(
-                        "Login", style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18,
-                        color: Colors.white,
-
-                      ),
-                      ),
-
-                    ),
-                  ),
-                ),
-
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const <Widget>[
-                    Text("Don't have an account?"),
-                    Text(" Sign up", style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 18,
-
+                  Text("Flutter Projects managed by NAID  & Sprints ",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 15,
                     ),)
-                  ],
-                ),
-              ],
-            ))
-          ],
+                  ,const SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(child: Image.asset('assets/sprints.png',scale: 0.6,)),
+                  Expanded(child: Image.asset('assets/naid.png',scale: 0.6,)),
+                  const SizedBox(height: 20)
+                ],
+              ),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  // the login button
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 40,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage()));
+                    },
+                    color: const Color(0xff0095FF),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: const Text(
+                      "1. Login Page",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                      ),
+                    ),
+                  ),
+                  // creating the signup button
+                  const SizedBox(height:10),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 40,
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=> const LoginPage()));
+                    },
+                    color: const Color(0xff0095FF),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: const Text(
+                      "2. Layout Exercise",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height:10),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 40,
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=> const LoginPage()));
+
+                    },
+                    color: const Color(0xff0095FF),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: const Text(
+                      "3. Page Navigation",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                      ),
+                    ),
+                  ),
+
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -117,42 +139,3 @@ class HomePage  extends StatelessWidget{
 
 }
 
-
-// we will be creating a widget for text field
-Widget inputFile({label, obscureText = false})
-{
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text(
-        label,
-        style: const TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.w400,
-            color:Colors.black87
-        ),
-
-      ),
-      const SizedBox(
-        height: 5,
-      ),
-      TextField(
-        obscureText: obscureText,
-        decoration: const InputDecoration(
-            contentPadding: EdgeInsets.symmetric(vertical: 0,
-                horizontal: 10),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                  color: Colors.grey
-              ),
-
-            ),
-            border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey)
-            )
-        ),
-      ),
-      const SizedBox(height: 10,)
-    ],
-  );
-}
